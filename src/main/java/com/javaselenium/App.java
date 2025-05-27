cbdd40e79e8374bcdab84ab04d50ecf4243fdbd5
Elementick <elementick@outlook.com>
2025-05-27 16:19:34 +0530

    inital setup

---
+++ src/main/java/com/javaselenium/App.java
@@ -0,0 +1,21 @@
+package com.javaselenium;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     * @throws InterruptedException 
+     */
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        Google testGoogle = new Google();
+
+        testGoogle.testGoogleSearch();
+    }
+}
---
+++ src/main/java/com/javaselenium/Google.java
@@ -0,0 +1,31 @@
+package com.javaselenium;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+public class Google {
+
+    // Make sure to have the ChromeDriver executable in your PATH or set the property below
+    // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
+
+    WebDriver driver = new org.openqa.selenium.chrome.ChromeDriver();
+   
+    public void testGoogleTitle(){
+    
+        this.driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + this.driver.getTitle());
+        this.driver.quit();
+    }
+
+    public void testGoogleSearch() throws InterruptedException{
+        this.driver.get("https://www.google.com");
+        //*[@id="APjFqb"]
+        this.driver.findElement(org.openqa.selenium.By.name("q")).sendKeys("Testing");
+        TimeUnit.SECONDS.sleep(5);
+
+    }
+
+
+}
